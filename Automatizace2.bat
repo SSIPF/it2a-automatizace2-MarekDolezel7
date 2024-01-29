5df4be7211e77764232ff1d69c67ae12294b9769
Your Name <you@example.com>
2024-01-29 11:13:07 +0100

    uziv

--- Automatizace2.bat
+++ Automatizace2.bat
@@ -1,29 +1,30 @@
 @echo off
 
-set "uzivFile="
-set "pswdDefault="
+set "file="
+set "pswd="
 
 :getopts
-if "%~1"=="" goto :uziv_Prepinac
-if i/ "%~1"=="-uziv.txt"(
-    set "uzivFile=%~2"
+if "%~1"=="" goto :file_Prepinac
+if i/ "%~1"=="-file" (
+    set "file=%~2"
     shift
     shift
     goto :getopts
 )
 
-:uziv_Prepinac
-if not defined uzivFile (
-    echo Prepinac -uziv.txt je povinny.
+:file_Prepinac
+if not defined file (
+    echo Prepinac -file je povinny.
 Konec skriptu.
     exit /b 1
 )
 
-for /f "tokens=1-4" %%a in (%uzivFile%) do (
+for /f "tokens=1-4" %%a in (%file%) do (
     set "jmeno=%%a"
     set "fullname=%%a %%b"
     set "pswd=%%c"
     set "group=%%d"
+    echo !jmeno! !fullname! !pswd! !group!
 
     net !jmeno! !pswd! /add /fullname: "!fullname!"
     net localgroup !group! !jmeno! /add
